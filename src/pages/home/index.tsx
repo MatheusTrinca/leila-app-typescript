9d841c1c4b536190d719dbca744f1fe72605e92e
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-06-12 16:16:43 -0300

    header

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { Container, Header } from './styles';
+import {
+  Container,
+  Header,
+  UserAvatar,
+  UserAvatarButton,
+  UserGreeting,
+  UserInfo,
+  UserInfoDetail,
+  UserName,
+  UserWrapper,
+  Icon,
+} from './styles';
+import defaultAvatar from '../../assets/avatar02.png';
 
 export const Home: React.FC = () => {
   return (
     <Container>
-      <Header />
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <UserAvatarButton onPress={() => {}}>
+              <UserAvatar source={defaultAvatar} />
+            </UserAvatarButton>
+            <UserInfoDetail>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Jorge</UserName>
+            </UserInfoDetail>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };

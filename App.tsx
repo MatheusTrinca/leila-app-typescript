0027b342b78f6f3b5bf7a4026f805ea8ab29b362
Matheus Trinca <matheusfelipetrinca@gmail.com>
2022-06-12 14:11:16 -0300

    header part1

--- App.tsx
+++ App.tsx
@@ -2,8 +2,23 @@ import React from 'react';
 import { Home } from './src/pages/home';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Home />
